ef46dc75aae4c0da83178e6f48689b16f01eefd2
mrkgnet <roztainfo@gmail.com>
2025-03-15 23:08:37 +0330

    v1.0.1_create_template_site

---
+++ user-panel/footer-user-panel.php
@@ -0,0 +1,57 @@
+<footer class="bg-stone-50 sm:mr-64  p-4">
+            <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+               <div class="">
+                  <a href="#" class="flex flex-col gap-2 my-3 items-center">
+                     <p class="text-sm text-gray-500 sm:text-center ">
+                        مارا در تلگرام دنبال کنید
+                     </p>
+                     <i class="fa-brands fa-telegram fa-beat-fade fa-lg my-3" style="color: #74C0FC;"></i>
+                  </a>
+               </div>
+
+
+               <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                  <div>
+                     <ul class="text-gray-500  font-medium">
+                        <li class="mb-4">
+                           <a href="#" class="hover:underline">خانه</a>
+                        </li>
+                        <li>
+                           <a href="#" class="hover:underline">Tailwind CSS</a>
+                        </li>
+                     </ul>
+                  </div>
+                  <div>
+                     <ul class="text-gray-500 dark:text-gray-400 font-medium">
+                        <li class="mb-4">
+                           <a href="#" class="hover:underline ">android</a>
+                        </li>
+                        <li>
+                           <a href="#" class="hover:underline">ios</a>
+                        </li>
+                     </ul>
+                  </div>
+
+               </div>
+            </div>
+            <hr class="my-6 border-gray-200 sm:mx-auto  lg:my-8" />
+            <div class="flex flex-col gap-2 mt-3 items-center">
+               <p class="text-sm text-gray-500 sm:text-center "> تمام حقوق مادی و معنوی محفوظ است.
+               </p>
+               <p> 2025©</p>
+            </div>
+
+   </footer>
+   <!-- script of site  -->
+   
+   <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+   <script>
+      $(".openSideBar").click(function(e) {
+         $('#content_sidebar').toggleClass('translate-x-full');
+
+      });
+   </script>
+
+</body>
+
+</html>
